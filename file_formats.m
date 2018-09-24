17db92451c7ac3c6ed2a22ba09bc82a20c396abb
dlanier <lanier4@illinois.edu>
2018-09-24 11:00:57 -0500

    stage one fini

---
+++ file_formats.m
@@ -0,0 +1,8 @@
+
+imdatadir = '/Users/mojo/git_clone/dlanier/kaggle_challenge_01/data/kaggle_train';
+
+fname1 = 'fded8107-fee3-486d-83f7-3f145cc22da6_R_NoLuOpNotNorm_Negative.png';
+fname1 = fullfile(imdatadir, fname1);
+disp(fname1)
+
+Im = imread(fname1);
